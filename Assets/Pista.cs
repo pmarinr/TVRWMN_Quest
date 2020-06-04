6d9635d5e00951f599eba784f72014933c7a9444
Pedro Marin <pmarinr@gmail.com>
2020-06-04 23:11:31 +0200

    Update Material Papel

---
+++ Assets/Pista.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pista : MonoBehaviour
+{
+    bool inPizarra = false;
+    Rigidbody rb;
+    public Transform posInicial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        if (posInicial != null) { 
+        transform.position = posInicial.position;
+        transform.rotation = posInicial.rotation;
+        transform.parent = posInicial;
+        }
+
+
+    }
+
+   
+
+}
